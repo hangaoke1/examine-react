002e7f293af438ed722e82480e7aaadaaf9b1aea
hangaoke <188869009@qq.com>
2022-03-29 17:00:49 +0800

    feat: test workflows

--- src/components/YsFlow/index.jsx
+++ src/components/YsFlow/index.jsx
@@ -83,10 +83,10 @@ const YsFlow = observer(() => {
         {/* 工具条 */}
         <div className="flow-editor-toolbar">
           <Button style={{ padding: '4px 5px'}} type="link" onClick={previewJSON}>
-            查看JSON
+            查看json
           </Button>
           <Button style={{ padding: '4px 5px'}} type="link" onClick={previewXML}>
-            查看XML
+            查看xml
           </Button>
           <Button
             icon={<MinusOutlined />}
